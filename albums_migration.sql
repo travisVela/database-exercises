02f6568de483e3a406958dfcdd9b8a5d0b83526c
Travis Vela <jtravisvela@gmail.com>
2019-04-08 16:20:36 -0500

    reconfigure sql file

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,7 +2,7 @@ USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
-CREATE TABLE IF NOT EXISTS albums(
+CREATE TABLE albums(
   id int UNSIGNED NOT NULL AUTO_INCREMENT,
   artist VARCHAR(50),
   name VARCHAR(50),

b332a0111645ff18e1449541dd0ba60cac0e1210
Travis Vela <jtravisvela@gmail.com>
2019-04-10 13:36:07 -0500

    add truncate statement

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
-  id int UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(50),
-  name VARCHAR(50),
-  release_date DATE,
-  sales FLOAT,
-  genre VARCHAR(50),
-  PRIMARY KEY (id)
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(50) NOT NULL,
+name VARCHAR(50) NOT NULL,
+release_date INT,
+sales DECIMAL(9, 5),
+genre VARCHAR(50),
+PRIMARY KEY (id)
 );

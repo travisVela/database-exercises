07ea577f55e7db73916235c15513540dafeda887
Travis Vela <jtravisvela@gmail.com>
2019-04-10 11:29:09 -0500

    add where exercises solutions part 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,16 +2,26 @@ use employees;
 
 select *
 from employees
-where first_name in ('Irena', 'Vidya', 'Maya');
+where ((first_name = 'Irena')
+or (first_name = 'Vidya')
+or (first_name = 'Maya'))
+and (gender = 'M');
 
 select *
 from employees
-where last_name like 'E%';
+where (last_name like 'E%')
+or (last_name like '%E');
 
 select *
 from employees
-where hire_date between '1990-01-01'
-  and '1999-12-31';
+where (last_name like 'E%')
+and (last_name like '%E');
+
+select *
+from employees
+where (hire_date between '1990-01-01'
+  and '1999-12-31')
+and (birth_date like '%-12-25');
 
 select *
 from employees
@@ -19,4 +29,5 @@ where birth_date like '%-12-25';
 
 select *
 from employees
-where last_name like '%q%'
+where (last_name like '%q%')
+and (last_name not like '%qu%');

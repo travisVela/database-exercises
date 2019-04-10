245054c017ef02cb4ee1b2efc5089b9be578b2c2
Travis Vela <jtravisvela@gmail.com>
2019-04-10 14:04:23 -0500

    reformat solutions

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,20 +2,20 @@ use employees;
 
 select *
 from employees
-where ((first_name = 'Irena')
-or (first_name = 'Vidya')
-or (first_name = 'Maya'))
-and (gender = 'M');
+where (first_name = 'Irena'
+or first_name = 'Vidya'
+or first_name = 'Maya')
+and gender = 'M';
 
 select *
 from employees
-where (last_name like 'E%')
-or (last_name like '%E');
+where last_name like 'E%'
+or last_name like '%E';
 
 select *
 from employees
-where (last_name like 'E%')
-and (last_name like '%E');
+where last_name like 'E%'
+and last_name like '%E';
 
 select *
 from employees
@@ -29,5 +29,5 @@ where birth_date like '%-12-25';
 
 select *
 from employees
-where (last_name like '%q%')
-and (last_name not like '%qu%');
+where last_name like '%q%'
+and last_name not like '%qu%';
